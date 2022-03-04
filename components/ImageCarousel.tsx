a675bb7d05157a73957318d2e6e9422207be53d6
Grzegorz Siedlecki <grzegorz.siedlecki@outlook.com>
2022-03-04 13:34:56 +0100

    better image carousel

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -1,10 +1,12 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/layout";
 import React from "react";
-import useEmblaCarousel from "embla-carousel-react";
+// import useEmblaCarousel from "embla-carousel-react";
 import { Image } from "@chakra-ui/image";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { Button } from "@chakra-ui/button";
 import useLightModeCheck from "../libs/hooks/useLightModeCheck";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 // import Image from "next/image"
 interface ImageCarouselProps {
   images: string[];
@@ -12,21 +14,22 @@ interface ImageCarouselProps {
 
 export default function ImageCarousel(props: ImageCarouselProps): JSX.Element {
   const isLightMode = useLightModeCheck();
-  const [emblaRef, emblaApi] = useEmblaCarousel({ containScroll: "keepSnaps" });
-  const [currentImage, setCurrentImage] = React.useState<number>(
-    emblaApi?.selectedScrollSnap() || 0
-  );
-  const [canScrollLeft, setCanScrollLeft] = React.useState<boolean>(
-    emblaApi ? emblaApi.canScrollPrev() : false
-  );
-  const [canScrollRight, setCanScrollRight] = React.useState<boolean>(
-    emblaApi ? emblaApi.canScrollNext() : props.images.length > 1 ? true : false
-  );
-  emblaApi?.on("select", () => {
-    setCanScrollLeft(emblaApi.canScrollPrev());
-    setCanScrollRight(emblaApi.canScrollNext());
-    setCurrentImage(emblaApi?.selectedScrollSnap());
-  });
+  // const [emblaRef, emblaApi] = useEmblaCarousel({ containScroll: "keepSnaps" });
+  // const [currentImage, setCurrentImage] = React.useState<number>(
+  //   emblaApi?.selectedScrollSnap() || 0
+  // );
+  // const [canScrollLeft, setCanScrollLeft] = React.useState<boolean>(
+  //   emblaApi ? emblaApi.canScrollPrev() : false
+  // );
+  // const [canScrollRight, setCanScrollRight] = React.useState<boolean>(
+  //   emblaApi ? emblaApi.canScrollNext() : props.images.length > 1 ? true : false
+  // );
+  // emblaApi?.on("select", () => {
+  //   setCanScrollLeft(emblaApi.canScrollPrev());
+  //   setCanScrollRight(emblaApi.canScrollNext());
+  //   setCurrentImage(emblaApi?.selectedScrollSnap());
+  // });
+
   function renderImages() {
     return props.images.map((image) => (
       <Image
@@ -34,7 +37,9 @@ export default function ImageCarousel(props: ImageCarouselProps): JSX.Element {
         src={image}
         alt="Image"
         pos="relative"
-        w="100vw"
+        h={["65vh", "65vh", "50vh"]}
+        // w="100vw"
+        // height={"-webkit-fit-content"}
         pointerEvents="all"
         objectFit="contain"
         // flex="0 0 100%"
@@ -42,7 +47,27 @@ export default function ImageCarousel(props: ImageCarouselProps): JSX.Element {
       ></Image>
     ));
   }
-  function renderArrow(type: "left" | "right") {
+
+  function renderLeftArrow(
+    clickHandler: () => void,
+    hasNext: boolean,
+    label: string
+  ) {
+    return renderArrow("left", clickHandler, hasNext, label);
+  }
+  function renderRightArrow(
+    clickHandler: () => void,
+    hasNext: boolean,
+    label: string
+  ) {
+    return renderArrow("right", clickHandler, hasNext, label);
+  }
+  function renderArrow(
+    type: "left" | "right",
+    clickHandler: () => void,
+    hasNext: boolean,
+    label: string
+  ) {
     return (
       <Button
         color="black"
@@ -50,39 +75,15 @@ export default function ImageCarousel(props: ImageCarouselProps): JSX.Element {
         alignItems="center"
         // flexDir="row"
         // h="100%"
-        visibility={
-          type == "left"
-            ? canScrollLeft
-              ? "visible"
-              : "hidden"
-            : canScrollRight
-            ? "visible"
-            : "hidden"
-        }
-        opacity={
-          type == "left"
-            ? canScrollLeft
-              ? "1"
-              : "0"
-            : canScrollRight
-            ? "1"
-            : "0"
-        }
+        visibility={hasNext ? "visible" : "hidden"}
+        opacity={hasNext ? "1" : "0"}
         // opacity={emblaApi?.canScrollNext() ? "1" : "0"}
         top="50%"
         transform="translateY(-50%)"
         variant="pill"
         pos="absolute"
         bg="white"
-        transparent="false"
-        onClick={() => {
-          if (emblaApi) {
-            if (type == "left") emblaApi.scrollPrev();
-            if (type == "right") emblaApi.scrollNext();
-            // setCanScrollLeft(emblaApi.canScrollPrev());
-            // setCanScrollRight(emblaApi.canScrollNext());
-          }
-        }}
+        onClick={clickHandler}
         w="14"
         h="14"
         left={type == "left" ? 0 : "auto"}
@@ -94,7 +95,12 @@ export default function ImageCarousel(props: ImageCarouselProps): JSX.Element {
       </Button>
     );
   }
-  function renderSizeBar() {
+  function renderSizeBar(
+    clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
+    isSelected: boolean,
+    index: number,
+    label: string
+  ) {
     function renderDot(id: number) {
       return (
         <Box
@@ -102,49 +108,48 @@ export default function ImageCarousel(props: ImageCarouselProps): JSX.Element {
           w="4"
           h="4"
           m="1"
-          transform={currentImage === id ? `scale(1.2)` : "scale(1.0)"}
+          transform={isSelected ? `scale(1.2)` : "scale(1.0)"}
           transition="0.3s"
           zIndex="5"
           backgroundColor={isLightMode ? "white" : "black"}
           border="2px solid"
           borderColor={isLightMode ? "black" : "white"}
           borderRadius="full"
+          value={index}
+          display="inline-block"
+          onClick={clickHandler}
+          cursor="pointer"
         ></Box>
       );
     }
-    return (
-      <Flex
-        mt="-10"
-        color="black"
-        justifySelf="center"
-        alignSelf="center"
-        justifyContent="center"
-      >
-        {props.images.map((image, index) => {
-          return renderDot(index);
-        })}
-      </Flex>
-    );
+    return renderDot(index);
   }
 
   return (
-    <Box h="inherit" borderRadius="inherit" pointerEvents="all">
-      <Box h="inherit" borderRadius="inherit" pointerEvents="all">
-        {renderArrow("left")}
-        <Box
-          ref={emblaRef}
-          pointerEvents="all"
-          h="inherit"
-          overflow="hidden"
-          borderRadius="inherit"
+    <Box
+      h="inherit"
+      borderRadius="inherit"
+      pointerEvents="all"
+      overflow={"hidden"}
+    >
+      <Box
+        h="inherit"
+        borderRadius="inherit"
+        pointerEvents="all"
+        overflow={"hidden"}
+      >
+        <Carousel
+          showArrows
+          showThumbs={false}
+          showStatus={false}
+          emulateTouch
+          renderArrowPrev={renderLeftArrow}
+          renderArrowNext={renderRightArrow}
+          renderIndicator={renderSizeBar}
         >
-          <Flex h="inherit" pointerEvents="all">
-            {renderImages()}
-          </Flex>
-        </Box>
-        {renderArrow("right")}
+          {renderImages()}
+        </Carousel>
       </Box>
-      {renderSizeBar()}
     </Box>
   );
 }
