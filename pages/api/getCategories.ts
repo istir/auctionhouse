5ad1c84ccf537e1c9f55ca96e1e99f6cb5af238d
Grzegorz Siedlecki <grzegorz.siedlecki@outlook.com>
2022-04-21 18:12:38 +0200

    fixed type error

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../prisma/prisma";
 
-
 import { Session } from "next-iron-session";
 // export default withSession(
 export default async function getCategories(
@@ -22,7 +21,7 @@ export default async function getCategories(
 
   const category = await prisma.category.findUnique({
     where: { id: categoryId },
-    include: { children: true },
+    // include: { children: true },
   });
   if (!category) {
     res.end("Wrong id");
