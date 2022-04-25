c43336d97b91be8b600f9865d4acb1cabd36f29a
Grzegorz Siedlecki <grzegorz.siedlecki@outlook.com>
2022-04-25 20:16:24 +0200

    removed random auction get

--- libs/getRandomAuctionsLib.ts
+++ libs/getRandomAuctionsLib.ts
@@ -7,44 +7,66 @@ export default async function getRandomAuctions(limit: number, count: number) {
     "categoryId" | "sellerId" | "markdown" | "buyerId"
   >[] = [];
 
-  for (let i = 0; i < limit; i += 1) {
-    const result = await prisma.auction.findFirst({
-      where: { dateEnd: { gt: Date.now().toString() } },
-      select: {
-        id: true,
-        bids: true,
-        name: true,
-        dateEnd: true,
-        price: true,
-        originalPrice: true,
-        image: true,
-        url: true,
-        timesBought: true,
-        usersBought: true,
-        bidding: true,
-      },
-      // include: { bids: true, },
-      take: 1,
-      skip: Math.floor(Math.random() * count),
-    });
-    if (result) {
-      results[i] = result;
-    }
-  }
+  //! for not, it returns latest
 
-  if (!results) {
-    return false;
-  }
-  //remove duplicates
-  const r1 = results.filter((result) => result);
-  const r = r1.filter((thing, index) => {
-    const _thing = JSON.stringify(thing);
-    return (
-      index ===
-      r1.findIndex((obj) => {
-        return JSON.stringify(obj) === _thing;
-      })
-    );
+  const result = await prisma.auction.findMany({
+    orderBy: { id: "desc" },
+    where: { dateEnd: { gt: Date.now().toString() } },
+    select: {
+      id: true,
+      bids: true,
+      name: true,
+      dateEnd: true,
+      price: true,
+      originalPrice: true,
+      image: true,
+      url: true,
+      timesBought: true,
+      usersBought: true,
+      bidding: true,
+    },
+    take: limit,
   });
-  return r;
+  // const itemsCount = await prisma.auction.count();
+  // const skip = Math.floor(Math.random() * itemsCount);
+  // for (let i = 0; i < limit; i += 1) {
+  //   const result = await prisma.auction.findFirst({
+  //     where: { dateEnd: { gt: Date.now().toString() } },
+  //     select: {
+  //       id: true,
+  //       bids: true,
+  //       name: true,
+  //       dateEnd: true,
+  //       price: true,
+  //       originalPrice: true,
+  //       image: true,
+  //       url: true,
+  //       timesBought: true,
+  //       usersBought: true,
+  //       bidding: true,
+  //     },
+  //     // include: { bids: true, },
+  //     take: 1,
+  //     skip,
+  //   });
+  //   if (result) {
+  //     results[i] = result;
+  //   }
+  // }
+  return result;
+  // if (!results) {
+  //   return false;
+  // }
+  // //remove duplicates
+  // const r1 = results.filter((result) => result);
+  // const r = r1.filter((thing, index) => {
+  //   const _thing = JSON.stringify(thing);
+  //   return (
+  //     index ===
+  //     r1.findIndex((obj) => {
+  //       return JSON.stringify(obj) === _thing;
+  //     })
+  //   );
+  // });
+  // return r;
 }
