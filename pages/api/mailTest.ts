4059f9c1ebe80ff625b8df1b3761e5a91bf0535d
Grzegorz Siedlecki <grzegorz.siedlecki@outlook.com>
2022-04-28 10:34:27 +0200

    mail test refresh env

--- pages/api/mailTest.ts
+++ pages/api/mailTest.ts
@@ -16,7 +16,7 @@ export default async function handler(
     service: process.env.EMAIL_PROVIDER,
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASSWORD },
   });
-
+  //aaa
   await new Promise((resolve, reject) => {
     // verify connection configuration
     transporter.verify(function (error, success) {
