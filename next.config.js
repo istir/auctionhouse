43914c8d1604bb6d5adb0b77011f6c278d5209f1
Grzegorz Siedlecki <grzegorz.siedlecki@outlook.com>
2022-05-19 17:00:41 +0200

    changed env variable

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const removeImports = require("next-remove-imports")({});
 module.exports = removeImports({
   reactStrictMode: true,
   publicRuntimeConfig: {
-    IBB_UPLOAD_URL: process.env.IBB_UPLOAD_URL,
+    IBB_UPLOAD_URL: `https://api.imgbb.com/1/upload?expiration=0&key=${process.env.IBB_API_KEY}`,
   },
   images: {
     domains: ["www.e3officefurniture.ca", "a.allegroimg.com"],
